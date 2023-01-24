cb65c19939cd2bd61723f283e2164f4a87902c1f
kflinnn <kflinncoding@gmail.com>
2023-01-24 17:52:03 -0500

    fixed some broken parts

--- Develop/index.js
+++ Develop/index.js
@@ -3,87 +3,43 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// generateREADME const
-const generateREADME = ({title, description, installation, usage, contribution, tests, license, questions, github, email}) => 
-`# ${title}
-
-## Description
-
-${description}
-
-## Table of Contents
-
--[Installation](#installation)
--[Usage](#usage)
--[Contribution](#contribution)
--[Tests](#tests)
--[License](#license)
--[Questions](#questions)
-
-## Installation
-
-${installation}
-
-## Usage
-
-${usage}
-
-## Contribution
-
-${contribution}
-
-## Tests
-
-${tests}
-
-## License
-
-${license}
-
-## Questions
-
-${questions}
-
-GitHub: ${github}
-
-Email: ${email};`
 
 //inquirer prompts
 inquirer
-    .prompt([
-       { type: 'input',
-        name: 'title',
-        message: 'What is the title of your README?',
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a short description explaining the what, why, and how of your project.',
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'Provide a step-by-step description of how to get the development environment running.',
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'If you ran tests on your project, provide the examples on how to run them here.',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide instructions and examples for use.',
-      },
-      {
-        type: 'input',
-        name: 'contribution',
-        message: 'Include guidelines indicating how other developers can contribute to your project here, if applicable',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        choices: [
+  .prompt([
+    { type: 'input',
+      name: 'title',
+      message: 'What is the title of your README?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description explaining the what, why, and how of your project.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide a step-by-step description of how to get the development environment running.',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'If you ran tests on your project, provide the examples on how to run them here.',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions and examples for use.',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Include guidelines indicating how other developers can contribute to your project here, if applicable',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      choices: [
             "MIT License",
             "GNU General Public License v3.0",
             "GNU Lesser General Public License v3.0",
@@ -94,33 +50,33 @@ inquirer
             "Eclipse Public License 2.0",
             "The Unlicense",
             ],
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Please provide your GitHub username',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Please provide your email address',
-      },
-      {
-        type: 'input',
-        name: 'questions',
-        message: 'Provide instructions on how to be contacted through GitHub and email.',
-      },
-    ])
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please provide your GitHub username',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please provide your email address',
+    },
+    {
+      type: 'input',
+      name: 'questions',
+      message: 'Provide instructions on how to be contacted through GitHub and email.',
+    },
+  ])
     .then((answers) => {
-        const readMeContent = generateREADME(answers);
+        const readMeContent = generateMarkdown(answers);
 
-        fs.writeFile('readme.md', readMeContent, (err) => 
+        fs.writeFile('README.md', readMeContent, (err) => 
         err ? console.log(err) : console.log ('Successfully created README.md!')
         );
     });
 
 // TODO: Create a function to initialize app
-function init() {generateREADME};
+function init() {generateMarkdown};
 
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,31 +1,89 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (data.license !== license) {
-    return "";
+  console.log(license);
+  if (!license) {
+    return ``;
   }
+  return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (data.license !== license) {
+  // switch(license){
+  //   case "MIT License":
+  //      return `[License Description](https://opensource.org/licenses/MIT)`
+  //      case "",
+  // }
+  if (!license) {
     return "";
   }
+  return `[License descriptions](https://opensource.org/licenses/${license})]`
+
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (data.license !== license) {
-    return "";
-  }
-}
+// function renderLicenseSection(license) {
+//   if (!license) {
+//     return "";
+// renderLicenseBadge
+//   }
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+  ## Description
+
+${data.description}
+
+## Table of Contents
+
+-[Installation](#installation)
+<br />
+-[Usage](#usage)
+<br />
+-[Contribution](#contribution)
+<br />
+-[Tests](#tests)
+<br />
+-[License](#license)
+<br />
+-[Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## License
+
+${data.license}
+${renderLicenseLink(data.license)}
+
+## Questions
+
+${data.questions}
+
+GitHub: https://github.com/${data.github}
+
+Email: ${data.email};
 `;
 }
 
